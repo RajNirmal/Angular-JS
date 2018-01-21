ad208670501fa17ae92ba33e1b46d0b36cfc1358
RajNirmal <nirmal497@gmail.com>
2018-01-21 22:05:04 +0530

    Added more components

--- angular-tour-of-heros/src/app/heros/heros.component.ts
+++ angular-tour-of-heros/src/app/heros/heros.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { hero }from '../hero';
+import {HEROES} from '../mock-heros';
 
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
+
 export class HerosComponent implements OnInit {
-
+  heros = HEROES;
   constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  selectedHero : hero;
+  onSelect(myHero : hero):void{
+    this.selectedHero = myHero;
   }
-
 }
+
